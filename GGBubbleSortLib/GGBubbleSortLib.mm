82047dcab3d6492049bc9c239d92cf774c3b3591
dujia <815319775@qq.com>
2016-07-11 15:31:14 +0800

    排序

--- GGBubbleSortLib/GGBubbleSortLib.mm
+++ GGBubbleSortLib/GGBubbleSortLib.mm
@@ -11,8 +11,8 @@
 @implementation GGBubbleSortLib
 + (void)startTest
 {
-    int arr[] = {25,24,15,54,67};
-    BubbleSort1(arr, 5);
+    int arr[] = {25,24,15,4,67};
+    BubbleSort2(arr, 5);
     for (int i = 0; i < 5; i++) {
         NSLog(@"%d",arr[i]);
     }
@@ -24,6 +24,24 @@ void swap1(int *left , int *right)
     *left = *right;
     *right = tmp;
 }
+
+void swap2(int &left , int &right)
+{
+    int tmp = left;
+    left = right;
+    right = tmp;
+}
+
+void swap3(int &left , int &right)
+{
+    if (&left != &right) {
+        left ^= right;
+        right ^= left;
+        left ^= right;
+    }
+}
+
+
 
 void BubbleSort1(int arr[], int num)
 {
@@ -37,6 +55,22 @@ void BubbleSort1(int arr[], int num)
     }
 }
 
+void BubbleSort2(int arr[] , int num)
+{
+    int k = num;
+    int j;
+    bool flag = true;
+    while (flag) {
+        flag = false;
+        for (j = 1; j < k; j++) {
+            if (arr[j - 1] > arr[j]) {
+                swap1(&arr[j - 1], &arr[j]);
+                flag = true;
+            }
+        }
+        k--;
+    }
+}
 
 
 
